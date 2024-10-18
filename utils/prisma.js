54d573a3ac4001c3b7adc76114fea2d82fd0fda3
Anin-Hasan <200921@ku.ac.bd>
2024-10-18 13:00:15 +0600

    exports session

--- utils/prisma.js
+++ utils/prisma.js
@@ -5,9 +5,11 @@ const prisma = new PrismaClient();
 const User = prisma.user;
 const File = prisma.file;
 const Folder = prisma.folder;
+const Session = prisma.session;
 
 module.exports = {
   User,
   File,
   Folder,
+  Session,
 };
